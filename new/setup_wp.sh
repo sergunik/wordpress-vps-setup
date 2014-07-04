4c5b850b5b5f37cabaaadab1dd1188cd1954c4f2
sergunik <sergunik@gmail.com>
2014-07-04 16:07:45 +0300

    Fix recursive param

--- new/setup_wp.sh
+++ new/setup_wp.sh
@@ -10,19 +10,19 @@ WPDBNAME=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_NAME | cut -d \' -f 4`
 WPDBUSER=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_USER | cut -d \' -f 4`;
 WPDBPASS=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`;
 
-echo "Create database '$WPDBNAME'"
+echo "Create database '$WPDBNAME' (need mysql root password)"
 mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $WPDBNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-echo "Create user '$WPDBUSER' with privileges"
+echo "Create user '$WPDBUSER' with privileges (need mysql root password)"
 mysql -u root -p -e "CREATE USER '$WPDBUSER'@'localhost' IDENTIFIED BY '$WPDBPASS'; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,LOCK TABLES ON $WPDBNAME.* TO '$WPDBUSER'@'localhost';"
 
-echo "Import data to database from dump"
+echo "Import data to database from dump (need mysql root password)"
 mysql -u root -p $WPDBNAME < $VHOST_DIR$SITE/www/dump.sql
 rm -rf $VHOST_DIR$SITE/www/dump.sql
 
-echo "Chmod dirs"
-chmod -r 755 $VHOST_DIR$SITE/www
-chmod -r 777 $VHOST_DIR$SITE/www/wp-content/uploads
+echo "Chmod dirs in '$VHOST_DIR$SITE/www'"
+chmod -R 755 $VHOST_DIR$SITE/www
+chmod -R 777 $VHOST_DIR$SITE/www/wp-content/uploads
 
 echo
 echo "Done)"
