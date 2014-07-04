b863070fc829af14a3c3146f1b1949efeb190d74
sergunik <sergunik@gmail.com>
2014-07-04 19:13:28 +0300

    security fix for copier

--- old/copier.sh
+++ old/copier.sh
@@ -8,12 +8,16 @@ clear
 echo
 echo "Getting params from wp-config.php"
 WPDBNAME=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_NAME | cut -d \' -f 4`;
+WPDBUSER=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_USER | cut -d \' -f 4`;
+WPDBPASS=`cat $VHOST_DIR$SITE/www/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`;
 
 echo "Create MySQL dump"
-mysqldump -u root -p ${WPDBNAME} > $VHOST_DIR$SITE/www/dump.sql
+mysqldump -u ${WPDBUSER} -p${WPDBPASS} ${WPDBNAME} > $VHOST_DIR$SITE/www/dump.sql
 
 echo "Copy files to ${REMOTE_HOST}"
 scp -r ${VHOST_DIR}${SITE}/www/* root@${REMOTE_HOST}:${VHOST_DIR}${SITE}/www/
+
+rm -r $VHOST_DIR$SITE/www/dump.sql
 
 echo
 echo "Done)"
